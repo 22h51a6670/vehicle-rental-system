7b4b7122be4627d9ebb92a89019ff4f574186d67
Gnaneswar Reddy <22h51a6670@cmrcet.ac.in>
2025-02-21 15:48:41 +0530

    java project

---
+++ Readme.md
@@ -0,0 +1,24 @@
+# Vehicle Rental System
+
+## Description
+The Vehicle Rental System is a simple Java console application that allows users to rent various types of vehicles by selecting a vehicle type and specifying the rental duration. The system calculates the total price based on the number of days the vehicle is rented.
+
+## Features
+- Uses **Abstraction** by defining an abstract `Vehicle` class.
+- Implements **Inheritance**, where different vehicle types inherit from `Vehicle`.
+- Demonstrates **Polymorphism** through the `Rentable` interface implemented by the `Reservation` class.
+- Ensures proper **user input validation** for vehicle selection and rental days.
+
+## How to Run
+1. Compile the Java program:
+   ```sh
+   javac VehicleRental.java
+   ```
+2. Run the java program:
+    ```sh
+    java VehicleRental.java
+    ```
+## Output
+![VehicleRental](output/1.png)
+![VehicleRental](output/2.png)
+![VehicleRental](image.png)
---
+++ VehicleRental.java
@@ -0,0 +1,142 @@
+import java.util.Scanner;
+
+// Abstraction: Define an abstract class for Vehicle
+abstract class Vehicle {
+    private String type;
+    private double pricePerDay;
+
+    public Vehicle(String type, double pricePerDay) {
+        this.type = type;
+        this.pricePerDay = pricePerDay;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getPricePerDay() {
+        return pricePerDay;
+    }
+}
+
+// Concrete classes inheriting from Vehicle (Inheritance)
+class Sedan extends Vehicle {
+    public Sedan() {
+        super("Sedan (5-Seater)", 2500);
+    }
+}
+
+class Hatchback extends Vehicle {
+    public Hatchback() {
+        super("Hatchback (5-Seater)", 2000);
+    }
+}
+
+class SUV extends Vehicle {
+    public SUV() {
+        super("SUV (7-Seater)", 3500);
+    }
+}
+
+class MUV extends Vehicle {
+    public MUV() {
+        super("MUV (7-Seater)", 4000);
+    }
+}
+
+class SportsBike extends Vehicle {
+    public SportsBike() {
+        super("Sports Bike", 1000);
+    }
+}
+
+class CruiserBike extends Vehicle {
+    public CruiserBike() {
+        super("Cruiser Bike", 800);
+    }
+}
+
+class Scooter extends Vehicle {
+    public Scooter() {
+        super("Scooter", 500);
+    }
+}
+
+// Interface to enforce rental operations (Abstraction & Polymorphism)
+interface Rentable {
+    double calculateTotalPrice();
+    String getDetails();
+}
+
+// Reservation class implementing Rentable
+class Reservation implements Rentable {
+    private Vehicle vehicle;
+    private int days;
+
+    public Reservation(Vehicle vehicle, int days) {
+        this.vehicle = vehicle;
+        this.days = days;
+    }
+
+    @Override
+    public double calculateTotalPrice() {
+        return vehicle.getPricePerDay() * days;
+    }
+
+    @Override
+    public String getDetails() {
+        return "\nReservation Details:" +
+               "\n---------------------" +
+               "\nVehicle: " + vehicle.getType() +
+               "\nDays: " + days +
+               "\nTotal Price: " + calculateTotalPrice() + " Rs\n";
+    }
+}
+
+public class VehicleRental {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to Vehicle Rental System");
+        System.out.println("--------------------------------");
+        System.out.println("Available Vehicles:");
+        System.out.println("1. Sedan (5-Seater) - 2500 Rs/day");
+        System.out.println("2. Hatchback (5-Seater) - 2000 Rs/day");
+        System.out.println("3. SUV (7-Seater) - 3500 Rs/day");
+        System.out.println("4. MUV (7-Seater) - 4000 Rs/day");
+        System.out.println("5. Sports Bike - 1000 Rs/day");
+        System.out.println("6. Cruiser Bike - 800 Rs/day");
+        System.out.println("7. Scooter - 500 Rs/day");
+        
+        Vehicle vehicle = null;
+        while (vehicle == null) {
+            System.out.print("Enter the vehicle type (1-7): ");
+            int vehicleChoice = scanner.nextInt();
+            switch (vehicleChoice) {
+                case 1 -> vehicle = new Sedan();
+                case 2 -> vehicle = new Hatchback();
+                case 3 -> vehicle = new SUV();
+                case 4 -> vehicle = new MUV();
+                case 5 -> vehicle = new SportsBike();
+                case 6 -> vehicle = new CruiserBike();
+                case 7 -> vehicle = new Scooter();
+                default -> System.out.println("Invalid choice. Please select again.");
+            }
+        }
+
+        int days;
+        while (true) {
+            System.out.print("Enter the number of days: ");
+            days = scanner.nextInt();
+            if (days > 0) {
+                break;
+            } else {
+                System.out.println("Invalid number of days. Please enter a positive number.");
+            }
+        }
+        
+        Reservation reservation = new Reservation(vehicle, days);
+        System.out.println(reservation.getDetails());
+        scanner.close();
+    }
+}
